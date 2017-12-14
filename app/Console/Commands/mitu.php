50ca9207bd4e0a5f5bd755d5e3eba3ce21f88031
sunwenzheng <sunwenzheng@anheng.com>
2017-12-14 20:35:49 +0800

    start console

---
+++ app/Console/Commands/mitu.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace App\Console\Commands;
+
+use Illuminate\Console\Command;
+
+class mitu extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'mitu:model';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'set type server or client';
+
+    /**
+     * Create a new command instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    /**
+     * Execute the console command.
+     *
+     * @return mixed
+     */
+    public function handle()
+    {
+        //
+        $model = env('MITU_MODEL');
+
+        $this->info($model);
+
+        $port = env('MITU_PORT');
+        $this->info($port);
+
+
+    }
+}
